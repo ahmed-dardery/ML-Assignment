535461256b18e5a3eb5a6d7aa62d72c05d17f8e5
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2021-01-07 06:17:08 +0200

    Fixed Integer Division, Added tree size, Added the excercising function required
    -(Integer Division in calculate_entropy function), cause a problem "divid by zero" so changed the type of the size from integer to float using float().
    
    -given some train ratios required to run each ratio 5 times and calculate the accuracy and tree size for each iteration. (get the max, mean and min values too)

--- Assignment 2/Part1.py
+++ Assignment 2/Part1.py
@@ -25,7 +25,8 @@ def split_to_train_test(df, train_ratio):
 
 def calculate_entropy(y):
     classes, count = np.unique(y, return_counts=True)
-    size = len(y)
+    size = float(len(y))
+
     entropy_value = np.sum(
         [(-count[i] / size) * np.log2(count[i] / size) for i in range(len(classes))]
     )
@@ -48,18 +49,20 @@ def calculate_information_gain(feat, y):
 def recurse(x, y, features):
     unique, cnt = np.unique(y, return_counts=True)
     if len(unique) <= 1:
-        return unique[0]
+        return unique[0], 0
     if len(features) == 0:
-        return unique[np.argmax(cnt)]
+        return unique[np.argmax(cnt)], 0
 
     gains = [calculate_information_gain(x[feat], y) for feat in features]
     optimal = features[np.argmax(gains)]
     node = TreeNode(optimal)
+    leaves_size = 0
     for choice in np.unique(x[optimal]):
         subset = x[optimal] == choice
-        node.nxt[choice] = recurse(x[subset], y[subset], [v for v in features if v != optimal])
+        node.nxt[choice], tmp_leave_size = recurse(x[subset], y[subset], [v for v in features if v != optimal])
+        leaves_size += tmp_leave_size
 
-    return node
+    return node, (leaves_size + 1)
 
 
 def generate_decision_tree(x, y):
@@ -83,7 +86,13 @@ def calculate_accuracy(tree, df):
     for i in range(len(df)):
         if tree.predict(df.iloc[i, 1:]) == df.iloc[i, 0]:
             total += 1
-    return total / len(df)
+    return total / (len(df) * 1.0)
+
+def calculate_size(tree):
+    print(tree.label)
+    for node in tree.nxt:
+        #print(node)
+        calculate_size(node)
 
 
 def testing():
@@ -91,20 +100,52 @@ def testing():
     tree = generate_decision_tree(train.iloc[:, 1:], train.iloc[:, 0])
     print("Training accuracy: ", calculate_accuracy(tree, train))
 
+def exercising(df, train_ratio, iterations):
+    train_accuracies = []
+    test_accuracies = []
+    tree_sizes = []
+
+    for i in range(iterations):
+        train, test = split_to_train_test(df, train_ratio)
+        majority = find_majority(train)
+        fill_in_unknowns(train, majority)
+        fill_in_unknowns(test, majority)
+        tree, tree_size = generate_decision_tree(train.iloc[:, 1:], train.iloc[:, 0])
+
+        train_accuracies.append(calculate_accuracy(tree, train))
+        test_accuracies.append(calculate_accuracy(tree, test))
+        tree_sizes.append(tree_size)
+
+        print("Training accuracy(" + str(train_ratio) + ": " + str(train_accuracies[i]))
+        print("Testing  accuracy(" + str(1 - train_ratio) + ": " + str(test_accuracies[i]))
+        print("Tree size: " + str(tree_size))
+
+    return train_accuracies, test_accuracies, tree_sizes
+
 
 def main():
-    df = pd.read_csv('part1_data/house-votes-84.data.txt', header=None)
-    train, test = split_to_train_test(df, 0.25)
-    # majority = find_majority(df)
-    # fill_in_unknowns(df, majority)
-    majority = find_majority(train)
-    fill_in_unknowns(train, majority)
-    fill_in_unknowns(test, majority)
-    tree = generate_decision_tree(train.iloc[:, 1:], train.iloc[:, 0])
+    df = pd.read_csv('house-votes-84.data.txt', header=None)
+    iterations = 5
+    ratios = [0.25, 0.30, 0.40, 0.50, 0.60, 0.70]
+    ratios_size = len(ratios)
 
-    print("Training accuracy: ", calculate_accuracy(tree, train))
-    print("Testing  accuracy: ", calculate_accuracy(tree, test))
+    print("Run 5 times: training Ratios(0.25, 0.30, 0.40, 0.50, 0.60,0.07)\n")
+    for i in range(ratios_size):
+        print("For Train Ratio(" + str(ratios[i]) + ")\n")
+        train_accuracies, test_accuracies, tree_sizes = exercising(df, ratios[i], iterations)
 
+        print("\nMin Train accuracy: " + str(np.min(train_accuracies)) +
+              "\nMax Train accuracy: " + str(np.max(train_accuracies)) +
+              "\nMean Train accuracy: " + str(np.mean(train_accuracies)))
+
+        print("Min Test accuracy: " + str(np.min(test_accuracies)) +
+              "\nMax Test accuracy: " + str(np.max(test_accuracies)) +
+              "\nMean Test accuracy: " + str(np.mean(test_accuracies)))
+
+        print("Min Tree size: " + str(np.min(tree_sizes)) +
+              "\nMax Tree size: " + str(np.max(tree_sizes)) +
+              "\nMean Tree size: " + str(np.mean(tree_sizes)))
+        print("---------------------------------------------------")
 
 if __name__ == '__main__':
     main()
